029b8cf13a6365c6bf10800762aac0acf9a994f1
LTNhoTin <tinle155@gmail.com>
2024-07-18 07:36:39 +0700

    Update ChatBot.jsx

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -109,7 +109,7 @@ function ChatBot(props) {
                 console.log(error);
                 setDataChat((prev) => [
                     ...prev,
-                    ['start', ['Lỗi, không thể kết nối với server', null, null]],
+                    ['start', ['Xin lỗi, hiện không thể kết nối với server. Vui lòng đợi một chút và hỏi lại nhé!', null, null]],
                 ]);
             } finally {
                 setIsLoad(false);
