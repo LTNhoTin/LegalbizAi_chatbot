e77e54260fc05fc60d32fef548b122601530d54b
LTNhoTin <tinle155@gmail.com>
2024-07-18 07:34:07 +0700

    :-1:

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -44,7 +44,7 @@ function ChatBot(props) {
     useEffect(() => {
         scrollToEndChat();
         inputRef.current.focus();
-    }, [isLoading]);
+    }, [chatHistory]);
 
     useEffect(() => {
         const interval = setInterval(() => {
